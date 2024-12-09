2e9b659d317980193ef577e14c79099496b40865
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-12-09 16:06:28 -0500

    changed design of favourite books

--- frontend/src/components/FavouriteBooks.tsx
+++ frontend/src/components/FavouriteBooks.tsx
@@ -1,27 +1,35 @@
 const FavouriteBooks = () => {
     const favouriteBooks = [
-      { title: "Lessons in Stoicism", cover: "http://books.google.com/books/content?id=y5GIDwAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
-      { title: "Hardship and Happiness", cover: "http://books.google.com/books/content?id=bgjGAgAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
-      { title: "Between Two Fires", cover: "http://books.google.com/books/content?id=2PO1MtB7E_0C&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
-      { title: "Orientalism", cover: "http://books.google.com/books/content?id=npF5BAAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
+      { id: 1, title: "Lessons in Stoicism", cover: "http://books.google.com/books/content?id=y5GIDwAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
+      { id: 2, title: "Hardship and Happiness", cover: "http://books.google.com/books/content?id=bgjGAgAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
+      { id: 3, title: "Between Two Fires", cover: "http://books.google.com/books/content?id=2PO1MtB7E_0C&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
+      { id: 4, title: "Orientalism", cover: "http://books.google.com/books/content?id=npF5BAAAQBAJ&printsec=frontcover&img=1&zoom=3&edge=curl&source=gbs_api" },
     ];
   
     return (
-      <div className="mt-8 border-4 border-back-raised p-2 rounded-lg">
-        <h2 className="text-xl font-bold text-secondary-strong mb-4">Favourite Books</h2>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-          {favouriteBooks.map((book, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <img
-                src={book.cover}
-                alt={book.title}
-                className="w-48 h-72 rounded-md"
-              />
-              <p className="mt-2 text-md text-secondary-weak text-center">{book.title}</p>
-            </div>
-          ))}
+
+        <div className="mb-12">
+          <h2 className="text-primary text-2xl font-bold mb-4">Favourite Books</h2>
+          <div className="flex justify-center items-center">
+          <div className="grid grid-cols-4 gap-4 2xl:w-[80%] mx-auto">
+            {favouriteBooks.map((book) => (
+              <div
+                key={book.id}
+                className="relative group bg-back-raised p-2 rounded-lg shadow-md hover:shadow-lg transition-all"
+              >
+                <img
+                  src={book.cover}
+                  alt={book.title}
+                  className="rounded-md object-cover"
+                />
+                <div className="absolute inset-0 bg-back-overlay bg-opacity-50 opacity-0 rounded-md group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                  <p className="text-white text-sm font-bold">{book.title}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          </div>
         </div>
-      </div>
     );
   };
   
