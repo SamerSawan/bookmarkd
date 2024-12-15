0f21fd1ef78632f2c3fe457828af0e7819e7d886
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-12-14 23:26:48 -0500

    modified create user end point to create the three default bookshelves

--- backend/internal/handlers/handlerCreateUser.go
+++ backend/internal/handlers/handlerCreateUser.go
@@ -1,11 +1,44 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/samersawan/bookmarkd/backend/internal/database"
 )
+
+func createDefaultShelves(db *database.Queries, user_id string) error {
+
+	readShelf, err := db.CreateShelf(context.Background(), "Read")
+	if err != nil {
+		return fmt.Errorf("Failed to create 'Read' shelf: %w", err)
+	}
+	currentlyReadingShelf, err := db.CreateShelf(context.Background(), "Currently Reading")
+	if err != nil {
+		return fmt.Errorf("Failed to create 'Currently Reading' shelf: %w", err)
+	}
+	toBeReadShelf, err := db.CreateShelf(context.Background(), "To Be Read")
+	if err != nil {
+		return fmt.Errorf("Failed to create 'To Be Read' shelf: %w", err)
+	}
+
+	_, err = db.AddShelfToUser(context.Background(), database.AddShelfToUserParams{UserID: user_id, ShelfID: readShelf.ID})
+	if err != nil {
+		return fmt.Errorf("Failed to add shelf to user: %w", err)
+	}
+	_, err = db.AddShelfToUser(context.Background(), database.AddShelfToUserParams{UserID: user_id, ShelfID: currentlyReadingShelf.ID})
+	if err != nil {
+		return fmt.Errorf("Failed to add shelf to user: %w", err)
+	}
+	_, err = db.AddShelfToUser(context.Background(), database.AddShelfToUserParams{UserID: user_id, ShelfID: toBeReadShelf.ID})
+	if err != nil {
+		return fmt.Errorf("Failed to add shelf to user: %w", err)
+	}
+	return nil
+
+}
 
 func (cfg *ApiConfig) CreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
@@ -37,6 +70,11 @@ func (cfg *ApiConfig) CreateUser(w http.ResponseWriter, r *http.Request) {
 	user, err := cfg.Db.CreateUser(r.Context(), database.CreateUserParams{Email: params.Email, Username: params.Username, ID: uid})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to create new user.", err)
+		return
+	}
+	err = createDefaultShelves(cfg.Db, user.ID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to create default bookshelves for user", err)
 		return
 	}
 	respondWithJSON(w, http.StatusCreated, User{ID: user.ID, Email: user.Email, CreatedAt: user.CreatedAt, UpdatedAt: user.UpdatedAt, Username: params.Username})
